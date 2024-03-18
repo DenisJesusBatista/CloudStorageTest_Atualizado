7f3cffa51d4dc1d487a7b51e4b3c6f2982f4dc38
DenisJesusBatista <denis.si2011@gmail.com>
2024-03-18 11:08:57 -0300

    Subindo com os arquivos para o repositorio.

---
+++ CloudStorageTest.Application/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
@@ -0,0 +1,50 @@
+﻿using CloudStorageTest.Domain.Entities;
+using CloudStorageTest.Domain.Storage;
+using FileTypeChecker.Extensions;
+using FileTypeChecker.Types;
+using Microsoft.AspNetCore.Http;
+
+namespace CloudStorageTest.Application.UseCases.Users.UploadProfilePhoto;
+
+public class UploadProfilePhotoUseCase
+
+{
+    /*readonly define que apenas o construtor que passa o valor.*/
+    private readonly IStorageService _storageService;
+
+    /*Comando parar criar construtor: ctor*/
+    public UploadProfilePhotoUseCase(IStorageService storageService)
+    {
+        _storageService = storageService;   
+
+    }
+
+
+    public void Execute(IFormFile file)
+    {
+
+        var fileStream = file.OpenReadStream();
+
+        var isImage = fileStream.Is<JointPhotographicExpertsGroup>();
+
+        if (isImage == false)
+            throw new Exception("The file is not an image.");
+
+        var user = GetFromDatabase();
+
+        _storageService.Upload(file, user);
+
+    }
+
+
+    private User GetFromDatabase() 
+    {
+        return new User
+        {
+            Id = 1,
+            Name = "Denis",
+            Email = "denis.si2011@gmail.com.."
+        };
+    
+    }
+}
---
+++ CloudStorageTest.Infrastructure/Storage/GoogleDriveStorageService.cs
@@ -0,0 +1,32 @@
+﻿using CloudStorageTest.Domain.Entities;
+using CloudStorageTest.Domain.Storage;
+using Google.Apis.Drive.v3;
+using Microsoft.AspNetCore.Http;
+
+namespace CloudStorageTest.Infrastructure.Storage;
+internal class GoogleDriveStorageService : IStorageService
+{
+    public string Upload(IFormFile file, User user)
+    {
+        var service = new DriveService();
+
+        var driveFile = new Google.Apis.Drive.v3.Data.File
+        {
+            Name = file.Name,
+            MimeType = file.ContentType,
+            //Pasta do googledriver: Parents
+           
+        };
+
+        var command = service.Files.Create(driveFile, file.OpenReadStream(), file.ContentType);
+        command.Fields = "id"; /*Esse comando garante que o id do retorno seja preenchido*/
+        var response = command.Upload();
+
+        if (response.Status is not Google.Apis.Upload.UploadStatus.Completed /*or Google.Apis.Upload.UploadStatus.NotStarted*/)
+            throw response.Exception;
+
+        return command.ResponseBody.Id;
+
+
+    }
+}
